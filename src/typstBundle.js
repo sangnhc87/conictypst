87291e112b010d610ae4d8a238e0b46e1f56a8da
sangnhc <sangbeau@gmail.com>
2026-05-01 13:57:55 +0700

    fix: load typst wasm from cdn

--- src/typstBundle.js
+++ src/typstBundle.js
@@ -3,7 +3,9 @@ export {
   createTypstRenderer,
   loadFonts,
 } from '@myriaddreamin/typst.ts/dist/esm/main.mjs'
-import compilerWasmUrl from '@myriaddreamin/typst-ts-web-compiler/pkg/typst_ts_web_compiler_bg.wasm?url'
-import rendererWasmUrl from '@myriaddreamin/typst-ts-renderer/pkg/typst_ts_renderer_bg.wasm?url'
 
-export { compilerWasmUrl, rendererWasmUrl }
+export const compilerWasmUrl =
+  'https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-web-compiler@0.7.0-rc2/pkg/typst_ts_web_compiler_bg.wasm'
+
+export const rendererWasmUrl =
+  'https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-renderer@0.7.0-rc2/pkg/typst_ts_renderer_bg.wasm'

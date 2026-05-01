16502c1ed96912cf2f4066de108b901050c45bb1
sangnhc <sangbeau@gmail.com>
2026-05-01 13:12:40 +0700

    ok

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,13 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
   plugins: [react(), wasm(), topLevelAwait()],
+  optimizeDeps: {
+    exclude: [
+      '@myriaddreamin/typst.ts',
+      '@myriaddreamin/typst-ts-web-compiler',
+      '@myriaddreamin/typst-ts-renderer',
+    ],
+  },
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
@@ -16,6 +23,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
+          if (id.endsWith('.wasm')) return
           if (id.includes('react')) return 'vendor-react'
           if (id.includes('@monaco-editor')) return 'vendor-monaco'
           if (id.includes('@myriaddreamin')) return 'vendor-typst'
